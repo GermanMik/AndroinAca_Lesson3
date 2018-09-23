306b28effaf2569a3d8d93c2e12ef70949e5278d
GermanMik <tiktak@yandex.ru>
2018-09-23 22:47:52 +0300

    Color fixes OnCall method

--- app/src/main/java/ru/androidacademy/lecture3/germanmikhaylov/MainActivity.java
+++ app/src/main/java/ru/androidacademy/lecture3/germanmikhaylov/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.androidacademy.lecture3.germanmikhaylov;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void message(View view) {
+        TextInputEditText editText=findViewById(R.id.text_input_send_message);
+        String webadres = editText.getText().toString();
+        Uri webpage = Uri.parse(webadres);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
