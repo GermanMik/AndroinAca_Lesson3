d445960b87c03347febd2b383bec51373c229e8f
GermanMik <tiktak@yandex.ru>
2018-09-24 23:54:47 +0300

    set layout from code

--- app/src/main/java/ru/androidacademy/lecture3/germanmikhaylov/MainActivity.java
+++ app/src/main/java/ru/androidacademy/lecture3/germanmikhaylov/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +23,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void javaTextView() {
-        TextView disclaimer=new TextView(this);
+        TextView disclaimer = new TextView(this);
         disclaimer.setText("© 2018 German Mikhaylov");
         disclaimer.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
-        disclaimer.setTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+        disclaimer.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        LinearLayout myLayout = findViewById(R.id.layout);
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, // width
+                ViewGroup.LayoutParams.WRAP_CONTENT); // height
+        linearLayoutParams.setMargins(16,0,16,0);
+        linearLayoutParams.gravity = Gravity.BOTTOM;
+        disclaimer.setLayoutParams(linearLayoutParams);
+        myLayout.addView(disclaimer);
     }
 
     public void onSendMessage(View view) {
@@ -36,5 +48,4 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
-
 }
